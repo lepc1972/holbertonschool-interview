c9759bd1d588f5e9b543ab2f938eb30adea05806
lepc1972 <vagrant@ubuntu>
2021-12-10 22:56:29 +0000

    lock

--- 0x00-lockboxes/0-lockboxes.py
+++ 0x00-lockboxes/0-lockboxes.py
@@ -7,6 +7,8 @@ def canUnlockAll(boxes):
     Return True if all boxes can be opened.
     Otherwise, return False.
     """
+    if not boxes:
+        return False
     keys = [0]
     for key in keys:
         for box in boxes[key]:
